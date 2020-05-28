1d91b442d1e5d35de0fdafcd153f4de1e7704f21
yangbin321 <1010545743@qq.com>
2020-05-28 18:57:37 +0800

    Spring框架学习之JDBC操作

---
+++ src/main/java/com/itheima/jdbc/JdbcDemo1.java
@@ -0,0 +1,47 @@
+package com.itheima.jdbc;
+
+import java.sql.*;
+
+/**
+ * @author yangbin
+ * @create 2019-10-04 0:02
+ *
+ * 程序的耦合
+ *      耦合：
+ *          类之间的依赖
+ *          方法间的依赖
+ *       解耦：
+ *          降低程序间的依赖关系
+ *       实际开发中：
+ *          应该做到：编译期不依赖，运行时才依赖。
+ *       解耦的思路：
+ *          第一步：使用反射创建对象，而避免使用new关键字。
+ *          第二步：通过读取配置文件来获取要创建的对象全限定类名。
+ *
+ */
+public class JdbcDemo1 {
+    public static void main(String[] args) throws Exception {
+
+        /**
+         * 连接JDBC数据库的操作
+         */
+        //1.注册驱动
+//        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //2.获取连接
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/eesy","root","123321");
+        //3.获取操作数据的预处理对象
+        PreparedStatement pstm = conn.prepareStatement("select * from  account");
+        //4.执行SQL，得到结果集
+        ResultSet rs = pstm.executeQuery();
+        //5.遍历结果集
+        while (rs.next()){
+            System.out.println(rs.getString("name"));
+        }
+        //6。释放资源
+        rs.close();
+        pstm.close();
+        conn.close();
+    }
+}
