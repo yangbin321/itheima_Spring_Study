9d394e286eddcefb08f5a6c382a5441419cfcd11
yangbin321 <1010545743@qq.com>
2020-05-28 20:14:52 +0800

    Spring_day04_eesy_05tx_xml

---
+++ src/main/java/com/itheima/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,50 @@
+package com.itheima.dao.impl;
+
+import com.itheima.damain.Account;
+import com.itheima.dao.IAccountDao;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import java.util.List;
+
+/**
+ * @author yangbin
+ * @create 2020-02-26 20:31
+ * 账户的持久层实现类
+ */
+public class AccountDaoImpl extends JdbcDaoSupport implements IAccountDao {
+
+    /**
+     * 记得补全
+     * @param accountId
+     * @return
+     */
+    public Account findAccountById(Integer accountId) {
+        List<Account> accounts = super.getJdbcTemplate().query("select * from account where id =?",new byte[]);
+        return accounts.isEmpty()?null:accounts.get(0);
+    }
+
+    /**
+     * 记得补全
+     * @param accountName
+     * @return
+     */
+    public Account findAccountByName(String accountName) {
+        List<Account> accounts = super.getJdbcTemplate().query("select * from account where name = ");
+        if (accounts.isEmpty()){
+            return null;
+        }
+        if (accounts.size()>1){
+            throw new RuntimeException("结果集不唯一");
+        }
+        return accounts.get(0);
+    }
+
+    /**
+     * 有错，一会儿记得修改
+     * @param account
+     */
+    public void updeteAccount(Account account) {
+        super.getJdbcTemplate().update("update account set name =? where id=?",account.getName());
+    }
+}
---
+++ src/test/java/com/itheima/test/Test.java
@@ -0,0 +1,9 @@
+package com.itheima.test;
+
+/**
+ * @author Yangbin
+ * @create 2020-02-29 12:36
+ * @Description
+ */
+public class Test {
+}
